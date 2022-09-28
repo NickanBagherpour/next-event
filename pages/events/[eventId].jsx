d3c7e7b114d29e6fc0fb7f1bfe036730527c4faa
Nickan <Nickan.bagherpour@gmail.com>
2022-09-28 08:34:52 +0330

    feat: work on event detail, layout

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,11 +1,40 @@
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../data/dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+
 const EventDetailPage = () => {
-  
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
     return (
-        <div>
-        <h1>Event Detail Page</h1>
-      </div>
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
     );
   }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+}
   
   export default EventDetailPage;
   
