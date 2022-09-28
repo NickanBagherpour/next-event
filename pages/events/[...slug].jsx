a72a943edaf654116c56bd51dee4b9cc69b6f5db
Nickan <Nickan.bagherpour@gmail.com>
2022-09-28 09:28:47 +0330

    feat: work on event filter, ...

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,10 +1,72 @@
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../data/dummy-data';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
+
 const FilteredEventsPage = () => {
-  
+  const router = useRouter();
+
+  const slug = router.query.slug;
+
+  if (!slug) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const slugYear = slug[0];
+  const slugMonth = slug[1];
+
+  const numYear = +slugYear;
+  const numMonth = +slugMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
     return (
-        <div>
-        <h1>Filtered Events Page</h1>
-      </div>
+      <>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
     );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
   }
   
   export default FilteredEventsPage;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
-import Link from "next/link";
+import { getFeaturedEvents } from '../data/dummy-data';
+import EventList from '../components/events/event-list';
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 };
